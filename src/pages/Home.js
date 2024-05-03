8c92d8fdae9af7a06d112f28230ee8f3a6560695
Kitui Kelly <kituikelly@gmail.com>
2024-05-03 20:40:53 +0300

    completes code along

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,31 @@
 import { useState, useEffect } from "react";
+
+/* Ok, let's try setting up another nested route. 
+As mentioned in our introduction, we want to view a specific user profile while still viewing the list of all our available users. We can implement this feature by making 
+our UserProfile component a nested route within our Home component. */
+import { Outlet, useOutletContext } from "react-router-dom";
 import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
 
 function Home() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:4000/users")
-      .then((r) => r.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error(error));
-  }, []);
-
+  /* Now, within our Home component
+   we can use the useOutletContext hook to access that piece of data:
+ */
+  const users = useOutletContext();
   const userList = users.map((user) => {
     return <UserCard key={user.id} user={user} />;
   });
 
   return (
     <>
-      <header>
-        {/* place NavBar here */}
-        <NavBar />
-      </header>
       <main>
         <h1>Home!</h1>
+        {/* If we provide a piece of data to the Outlet component within 
+        our App, and we want to access it within our UserProfile component,
+         we'll have to pass that data to the Outlet component within our 
+         Home component first.
+
+ */}
+        <Outlet context={users} />
         {userList}
       </main>
     </>
